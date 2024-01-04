7a1dfd64fe62e5494ba4ca9e3124ed6bf5b9f3f0
Harry Chou <harryyc.chou@gmail.com>
2024-01-03 17:42:24 -0800

    refactor question-0242

--- src/0242_valid_anagram/main.cc
+++ src/0242_valid_anagram/main.cc
@@ -13,29 +13,17 @@ using std::unordered_map;
 class Solution {
  public:
   bool isAnagram(string s, string t) {
-    unordered_map<string, int> m_s, m_t;
+    unordered_map<char, int> map_s, map_t;
 
-    for (int i = 0; i < s.length(); i++) {
-      string s_tmp = s.substr(i,1);
-      if (m_s.find(s_tmp) == m_s.end()) {
-        // create new element for map
-        m_s[s_tmp] = 1;
-      } else {
-        m_s[s_tmp] += 1;
-      }
-    }
-      
-    for (int i = 0; i < t.length(); i++) {
-      string s_tmp = t.substr(i,1);
-      if (m_t.find(s_tmp) == m_t.end()) {
-        // create new element for map
-        m_t[s_tmp] = 1;
-      } else {
-        m_t[s_tmp] += 1;
-      }
+    if (s.length() != t.length()) return false;
+
+    int len = s.length();
+    for (int i = 0; i < len; i++) {
+      map_s[s[i]] += 1;
+      map_t[t[i]] += 1;
     }
 
-    return (m_s == m_t);
+    return (map_s == map_t);
   }
 };
 
