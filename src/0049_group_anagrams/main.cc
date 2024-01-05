e706a026f23f439bca3f2a9b27534b4c9b72224d
Harry Chou <harryyc.chou@gmail.com>
2024-01-04 16:54:24 -0800

    using sort instead of creating hash string by for loop

--- src/0049_group_anagrams/main.cc
+++ src/0049_group_anagrams/main.cc
@@ -5,45 +5,29 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
 using std::string;
-using std::to_string;
 using std::vector;
 using std::unordered_map;
+using std::sort;
 
 class Solution {
  public:
   vector<vector<string>> groupAnagrams(const vector<string>& strs) {
     unordered_map<string, vector<string>> map;
     for (const auto& s : strs) {
-      map[to_hash_string(s)].push_back(s);
+      string sorted_str = s;
+      sort(sorted_str.begin(), sorted_str.end());
+      map[sorted_str].push_back(s);
     }
 
     // return result
     vector<vector<string>> ret;
     for (const auto& pair : map) {
       ret.push_back(pair.second);
-    }
-
-    return ret;
-  }
-
- private:
-  string to_hash_string(const string& str) {
-    int len = str.length();
-    vector<int> count(26);
-
-    // calculate times of each character
-    for (int i = 0; i < len; i++) {
-      count[str[i]-'a'] += 1;
-    }
-
-    // count to hash string
-    string ret;
-    for (int i = 0; i < 26; i++) {
-      ret.append(to_string(count[i])+"|");
     }
 
     return ret;
