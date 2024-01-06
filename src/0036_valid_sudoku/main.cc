fca48115807439d500b11322672d6e3788269e6d
Harry Chou <harryyc.chou@gmail.com>
2024-01-05 20:35:39 -0800

    add question-0036

---
+++ src/0036_valid_sudoku/main.cc
@@ -0,0 +1,138 @@
+/*
+ *  2024 Copyright Harry Chou
+ */
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::unordered_set;
+
+class Solution {
+ public:
+  bool isValidSudoku(const vector<vector<char>>& board) {
+    // check each row
+    for (const vector<char>& line : board) {
+      if (!isValidOneLine(line)) return false;
+    }
+
+    vector<char> line;
+    // check each column
+    for (int i = 0; i < 9; i++) {
+      line.clear();
+      // create a temp line
+      for (int j = 0; j < 9; j++) {
+        // i = row
+        // j = column
+        line.push_back(board[j][i]);
+      }
+      if (!isValidOneLine(line)) return false;
+    }
+
+    // check each 3x3 sub-box
+    //vector<char> line;
+    for (int i = 0; i < 9; i+=3) {
+      // create a temp line
+      for (int j = 0; j < 9; j+=3) {
+        line.clear();
+        // i = row
+        // j = column
+
+        // for sub-box
+        for (int k = 0; k < 3; k++) {
+          for (int l = 0; l < 3; l++) {
+            // k = sub box row
+            // l = sub box column
+            line.push_back(board[j+k][i+l]);
+          }
+        }
+        if (!isValidOneLine(line)) return false;
+      }
+    }
+   
+    return true;
+  }
+
+ private:
+  bool isValidOneLine(const vector<char>& line) {
+    unordered_set<char> s;
+    for (const auto& c : line) {
+      if (c != '.') {
+        if (s.find(c) != s.end()) {
+          // debug
+          //for (const auto& c : line) {
+          //  cout << c << " ";
+          //}
+          //cout << " <== false" << endl;
+
+          return false;
+        }
+        s.insert(c);
+      }
+    }
+
+    return true;
+  }
+};
+
+void print_ret(const bool& ret) {
+  if (ret) {
+    cout << "true" << endl;
+  } else {
+    cout << "false" << endl;
+  }
+}
+
+int main() {
+  Solution s;
+  vector<vector<char>> board;
+  bool ret;
+
+  // example 1
+  board.clear();
+  board = 
+      {{'5','3','.','.','7','.','.','.','.'}
+      ,{'6','.','.','1','9','5','.','.','.'}
+      ,{'.','9','8','.','.','.','.','6','.'}
+      ,{'8','.','.','.','6','.','.','.','3'}
+      ,{'4','.','.','8','.','3','.','.','1'}
+      ,{'7','.','.','.','2','.','.','.','6'}
+      ,{'.','6','.','.','.','.','2','8','.'}
+      ,{'.','.','.','4','1','9','.','.','5'}
+      ,{'.','.','.','.','8','.','.','7','9'}};
+  ret = s.isValidSudoku(board);
+  print_ret(ret);
+
+  // example 2
+  board.clear();
+  board = 
+      {{'8','3','.','.','7','.','.','.','.'}
+      ,{'6','.','.','1','9','5','.','.','.'}
+      ,{'.','9','8','.','.','.','.','6','.'}
+      ,{'8','.','.','.','6','.','.','.','3'}
+      ,{'4','.','.','8','.','3','.','.','1'}
+      ,{'7','.','.','.','2','.','.','.','6'}
+      ,{'.','6','.','.','.','.','2','8','.'}
+      ,{'.','.','.','4','1','9','.','.','5'}
+      ,{'.','.','.','.','8','.','.','7','9'}};
+  ret = s.isValidSudoku(board);
+  print_ret(ret);
+
+  board.clear();
+  board = 
+      {{'.','.','.','.','5','.','.','1','.'}
+      ,{'.','4','.','3','.','.','.','.','.'}
+      ,{'.','.','.','.','.','3','.','.','1'}
+      ,{'8','.','.','.','.','.','.','2','.'}
+      ,{'.','.','2','.','7','.','.','.','.'}
+      ,{'.','1','5','.','.','.','.','.','.'}
+      ,{'.','.','.','.','.','2','.','.','.'}
+      ,{'.','2','.','9','.','.','.','.','.'}
+      ,{'.','.','4','.','.','.','.','.','.'}};
+  ret = s.isValidSudoku(board);
+  print_ret(ret);
+
+  return 0;
+}
