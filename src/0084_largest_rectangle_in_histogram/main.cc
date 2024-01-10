7069cac2497e5c7143005e1193f6c6daf007082f
Harry Chou <harryyc.chou@gmail.com>
2024-01-09 19:42:21 -0800

    add method 2 for question-0084

--- src/0084_largest_rectangle_in_histogram/main.cc
+++ src/0084_largest_rectangle_in_histogram/main.cc
@@ -5,14 +5,47 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <stack>
 
 using std::cout;
 using std::endl;
 using std::vector;
-using std::min;
+using std::pair;
+using std::make_pair;
+using std::stack;
+using std::max;
 
 class Solution {
  public:
+  int largestRectangleArea2(const vector<int>& heights) {
+    int max_area = 0;
+    // index, height
+    stack<pair<int, int>> stk;
+
+    for (int i = 0; i < heights.size(); i++) {
+      int start = i;
+      while (!stk.empty() && (heights[i] < stk.top().second)) {
+        // decrease -> calculate area
+        pair<int, int> s = stk.top();
+        int area = (i - s.first) * s.second;
+        if (area > max_area) max_area = area;
+        stk.pop();
+        start = s.first;
+      }
+      stk.push(make_pair(start, heights[i]));
+    }
+
+    int end = heights.size();
+    while (!stk.empty()) {
+      pair<int, int> s = stk.top();
+      int area = (end - s.first) * s.second;
+      if (area > max_area) max_area = area;
+      stk.pop();
+    }
+
+    return max_area;
+  }
+
   int largestRectangleArea(const vector<int>& heights) {
     return find_max_area(0, heights.size()-1, heights);
   }
@@ -24,6 +57,7 @@ class Solution {
     } else {
       // find the minimum height (bottleneck)
       int min_height = heights[start];  // local minimum
+      int max_height = heights[start];  // local minimum
       int index = start;
 
       for (int i = start; i < end+1; i++) {
@@ -31,9 +65,17 @@ class Solution {
           min_height = heights[i];
           index = i;
         }
+
+        if (heights[i] > max_height) {
+          max_height = heights[i];
+        }
       }
 
       max_area = min_height * (end - start + 1);
+
+      // all elements are the same height
+      // dont need to seperate elements
+      if (min_height == max_height) return max_area;
 
       // seperate to two group
       // find the index of min_height
@@ -74,19 +116,37 @@ int main() {
   // example 1
   heights.clear();
   heights = {2, 1, 5, 6, 2, 3};
-  ret = s.largestRectangleArea(heights);
+  ret = s.largestRectangleArea2(heights);
   print_ret(ret);
 
   // example 2
   heights.clear();
   heights = {2, 4};
-  ret = s.largestRectangleArea(heights);
+  ret = s.largestRectangleArea2(heights);
   print_ret(ret);
 
   // example 3
   heights.clear();
-  heights = {4, 2};
-  ret = s.largestRectangleArea(heights);
+  heights = {0, 9};
+  ret = s.largestRectangleArea2(heights);
+  print_ret(ret);
+
+  // example 4
+  heights.clear();
+  heights = {2, 1, 2};
+  ret = s.largestRectangleArea2(heights);
+  print_ret(ret);
+
+  // example 5
+  heights.clear();
+  heights = {3, 6, 5, 7, 4, 8, 1, 0};
+  ret = s.largestRectangleArea2(heights);
+  print_ret(ret);
+
+  // example 6
+  heights.clear();
+  heights = {5, 5, 1, 7, 1, 1, 5, 2, 7, 6};
+  ret = s.largestRectangleArea2(heights);
   print_ret(ret);
 
   return 0;
