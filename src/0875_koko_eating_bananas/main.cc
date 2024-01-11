c2d93de1880c48b00df74c434bafca98f93ccab6
Harry Chou <harryyc.chou@gmail.com>
2024-01-10 16:42:23 -0800

    add question-0875

---
+++ src/0875_koko_eating_bananas/main.cc
@@ -0,0 +1,71 @@
+/*
+ *  2024 Copyright Harry Chou
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::sort;
+
+class Solution {
+ public:
+  int minEatingSpeed(const vector<int>& piles, int h) {
+    // mininum hour 1 <= min_h <= max(piles)
+    vector<int> spiles = piles;
+    sort(spiles.begin(), spiles.end());
+
+    int speed_l = 1;
+    int speed_r = spiles[spiles.size()-1];
+    int speed_mid;
+    while (speed_l<speed_r) {
+      speed_mid = (speed_l+speed_r)/2;
+      
+      int hours = 0;
+      for (auto const& n : piles) {
+        hours += (n-1)/speed_mid + 1;
+      }
+
+      // accepted speed
+      if (hours <= h) {
+        speed_r = speed_mid;
+      } else {
+        speed_l = speed_mid + 1;
+      }
+    }
+
+    return speed_l;
+  }  
+};
+
+void print_ret(const int& ret) {
+  cout << ret << endl;
+}
+
+int main() {
+  Solution s;
+  int ret;
+  vector<int> piles;
+
+  // example 1
+  piles.clear();
+  piles = {3, 6, 7, 11};
+  ret = s.minEatingSpeed(piles, 8);
+  print_ret(ret);
+
+  // example 2
+  piles.clear();
+  piles = {30, 11, 23, 4, 20};
+  ret = s.minEatingSpeed(piles, 5);
+  print_ret(ret);
+
+  // example 3
+  piles.clear();
+  piles = {30, 11, 23, 4, 20};
+  ret = s.minEatingSpeed(piles, 6);
+  print_ret(ret);
+
+  return 0;
+}
