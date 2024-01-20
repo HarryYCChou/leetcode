74fdb44ce12cd765d1ba41f884103346180477ed
Harry Chou <harryyc.chou@gmail.com>
2024-01-19 19:31:18 -0800

    add question-0543

---
+++ src/0543_diameter_of_binary_tree/main.cc
@@ -0,0 +1,60 @@
+/*
+ *  2024 Copyright Harry Chou
+ */
+#include <iostream>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::max;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) :
+                                  val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  int diameterOfBinaryTree(TreeNode* root) {
+    if (root == nullptr) return 0;
+    int root_d = maxDepth(root->left) + maxDepth(root->right);
+    int ret = max(root_d, diameterOfBinaryTree(root->left));
+    ret = max(ret, diameterOfBinaryTree(root->right));
+    return ret;
+  }
+
+  int maxDepth(TreeNode* root) {
+    if (root == nullptr) return 0;
+    return (1 + max(maxDepth(root->left), maxDepth(root->right)));
+  }
+};
+
+void print_ret(const int& ret) {
+  cout << ret << endl;
+}
+
+int main() {
+  Solution s;
+  TreeNode* head;
+  int ret;
+
+  // example 1
+  head = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                        new TreeNode(3));
+  ret = s.diameterOfBinaryTree(head);
+  print_ret(ret);
+
+  // example 2
+  head = new TreeNode(1, nullptr, new TreeNode(2));
+  ret = s.diameterOfBinaryTree(head);
+  print_ret(ret);
+
+  // example 3
+
+  return 0;
+}
