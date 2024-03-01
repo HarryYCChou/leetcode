36a9fde1152a17a56a485ad0cb41f583886c200e
Harry Chou <harryyc.chou@gmail.com>
2024-03-01 15:49:16 -0800

    refactory by Dijkstra's algorithm

--- src/0743_network_delay_time/main.cc
+++ src/0743_network_delay_time/main.cc
@@ -8,6 +8,7 @@
 #include <queue>
 #include <utility>
 #include <algorithm>
+#include <climits>
 
 using std::cout;
 using std::endl;
@@ -18,10 +19,58 @@ using std::priority_queue;
 using std::pair;
 using std::greater;
 using std::max;
+using std::make_pair;
 
 class Solution {
  public:
   int networkDelayTime(const vector<vector<int>>& times,
+                        const int& n,
+                        const int& k) {
+    vector<int> minTime(n + 1, INT_MAX);
+    // node[node_num] (time, next_node)
+    vector<pair<int, int>> node[n + 1];
+    // pq(time, next_node)
+    priority_queue<pair<int, int>,
+                  vector<pair<int, int>>,
+                  greater<pair<int, int>>> pq;
+    for (int i = 0; i < times.size(); i++) {
+      int from = times[i][0];
+      int to = times[i][1];
+      int time = times[i][2];
+      node[from].push_back(make_pair(time, to));
+    }
+    pq.push(make_pair(0, k));
+    minTime[k] = 0;
+
+    while (!pq.empty()) {
+      int time = pq.top().first;
+      int cur_node = pq.top().second;
+      pq.pop();
+      if (time > minTime[cur_node]) {
+        continue;
+      }
+
+      for (int i = 0; i < node[cur_node].size(); i++) {
+        int next_time = node[cur_node][i].first;
+        int next_node = node[cur_node][i].second;
+        if (next_time + minTime[cur_node] < minTime[next_node]) {
+          minTime[next_node] = next_time + minTime[cur_node];
+          pq.push(make_pair(next_time, next_node));
+        }
+      }
+    }
+
+    int ret = -1;
+    for (int i = 1; i <= n; i ++) {
+      cout << "i" << i << ":" << minTime[i] << endl;
+      ret = max(ret, minTime[i]);
+    }
+    if (ret == INT_MAX) return -1;
+
+    return ret;
+  }
+
+  int networkDelayTime2(const vector<vector<int>>& times,
                         const int& n,
                         const int& k) {
     unordered_map<int, int> traveled;
@@ -78,6 +127,9 @@ int main() {
   print_ret(ret);
 
   // example 3
+  times = {{1, 2, 1}};
+  ret = s.networkDelayTime(times, 2, 2);
+  print_ret(ret);
 
   return 0;
 }
