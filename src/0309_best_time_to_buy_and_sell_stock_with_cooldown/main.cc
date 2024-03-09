a310007e76ec63087ad42a0e579ffcff24eebbb4
Harry Chou <harryyc.chou@gmail.com>
2024-03-09 10:20:53 -0800

    add 2D DP state machine method for question-309

--- src/0309_best_time_to_buy_and_sell_stock_with_cooldown/main.cc
+++ src/0309_best_time_to_buy_and_sell_stock_with_cooldown/main.cc
@@ -4,15 +4,55 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using std::cout;
 using std::endl;
 using std::vector;
 using std::max;
+using std::unordered_map;
 
 class Solution {
  public:
+  // 2d DP + state machine method
   int maxProfit(const vector<int>& prices) {
+    unordered_map<int, int> no_stock;
+    unordered_map<int, int> one_stock;
+
+    return dfs(no_stock, one_stock, prices, false, 0, 0);
+  }
+
+  int dfs(unordered_map<int, int>& no_stock,
+          unordered_map<int, int>& one_stock,
+          const vector<int>& prices,
+          bool has_stock,
+          int day,
+          int profit) {
+    if (day >= prices.size()) return 0;
+    if (has_stock && one_stock.find(day) != one_stock.end()) {
+      return one_stock[day];
+    }
+    if (!has_stock && no_stock.find(day) != no_stock.end()) {
+      return no_stock[day];
+    }
+
+    if (has_stock) {
+      int sell = dfs(no_stock, one_stock, prices,
+                    !has_stock, day + 2, profit + prices[day]) + prices[day];
+      int hold = dfs(no_stock, one_stock, prices, has_stock, day + 1, profit);
+      one_stock[day] = max(sell, hold);
+      return one_stock[day];
+    } else {
+      int buy = dfs(no_stock, one_stock, prices, !has_stock, day + 1,
+                    profit - prices[day]) - prices[day];
+      int hold = dfs(no_stock, one_stock, prices, has_stock, day + 1, profit);
+      no_stock[day] = max(buy, hold);
+      return no_stock[day];
+    }
+  }
+
+  // 2D DP
+  int maxProfit2(const vector<int>& prices) {
     int n = prices.size();
     int dp[n + 2][n + 2];
     for (int i = 0; i < n + 2; i++) {
